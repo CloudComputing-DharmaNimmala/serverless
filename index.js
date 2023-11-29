665ea15fe24364cc2be79d95fc4732b009c75803
Dharma Thanishq Nimmala <nimmala.d@northeastern.edu>
2023-11-29 05:05:32 -0500

    adding serverless

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ import fetch from 'node-fetch';
 import FormData from 'form-data';
 import Mailgun from 'mailgun.js';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const gcp_key = process.env.gcpkey
-const parsedKey = JSON.parse(gcp_key);
-// const s3 = new AWS.S3({ region: 'us-west-1' });
+
+const gcp_key = process.env.GCP_KEY
+const bucketname = process.env.GCS_BUCKET_NAME
+const mailgunkey = process.env.MAILGUN_API_KEY
+const domain = process.env.MAILGUN_DOMAIN
+const dynamodb_table = process.env.DYNAMODB_NAME
+const decode_key = Buffer.from(gcp_key, 'base64')
+const parsedKey = JSON.parse(decode_key.toString('utf-8'));
+//const s3 = new AWS.S3({ region: 'us-west-1' });
+
 const googleStorage = new Storage({
   credentials : parsedKey
 });
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 export const handler = async (event, context) => {
   const snsMessage = JSON.parse(event.Records[0].Sns.Message);
@@ -25,29 +32,18 @@ export const handler = async (event, context) => {
       const fileBuffer = Buffer.from(new Uint8Array(fileData));
 
       //gcp bucket
-      const bucket = googleStorage.bucket('nimmalabucket1')
-      const file = bucket.file('file.zip')
+      const bucket = googleStorage.bucket(bucketname)
+      const file = bucket.file(`${emailFetch}/file.zip`)
       await file.save(fileBuffer, { contentType: 'text/plain' });      
       console.log('File uploaded to GCS');
-      //s3 bucket
-      // const params = {
-      //   Bucket: 'nimmalabucket1', // Replace with your bucket name
-      //   Key: 'file.zip', // Name for the file in the bucket
-      //   Body: new Uint8Array(fileData),
-      // };
-
-      // const uploadResult = await s3.upload(params).promise();
-
-      // console.log('File uploaded to S3:', uploadResult);
-      // Send email using Mailgun after successful upload
       const mailgun = new Mailgun(FormData);
-      const mg = mailgun.client({username: 'api', key: '680e8f7f873072a6c4ca1d5d15deced4-30b58138-c475eb19'});
+      const mg = mailgun.client({username: 'api', key: mailgunkey});
 
-      await mg.messages.create('mynscc.me', {
+      await mg.messages.create(domain, {
         from: "postmaster@mynscc.me",
-        to: ["dharmathanishqnimmala@gmail.com"],
+        to: [emailFetch],
         subject: "Hello",
-        text: "File Upload Success",
+        text: `File ${urlToFetch} Uploaded Successfully`,
         html: "<h1>The file has been successfully uploaded to S3.</h1>"
       })
       .then(msg => console.log(msg)) // logs response data
@@ -55,7 +51,7 @@ export const handler = async (event, context) => {
 
   // Store email information in DynamoDB
   const emailInfo = {
-    TableName: 'table1', // Replace with your DynamoDB table name
+    TableName: dynamodb_table, // Replace with your DynamoDB table name
     Item: {
       emailId: emailFetch, // Unique identifier for the email
       timestamp: new Date().toISOString(), // Timestamp of when the email was sent
